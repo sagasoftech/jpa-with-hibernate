f3c54cb8f89f62133e3735e0b15fe864fc5e9871
Sagar Dighe <sagasoftech@gmail.com>
2024-03-05 22:07:10 -0600

    first commit

---
+++ src/main/java/com/sagasoftech/jpa/hibernate/JpaWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWithHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWithHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagasoftech/jpa/hibernate/JpaWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagasoftech.jpa.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
