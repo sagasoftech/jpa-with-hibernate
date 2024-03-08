8ded92fad0a1aae3e6f5ee2030c9f2b4f378a879
Sagar Dighe <sagasoftech@gmail.com>
2024-03-08 00:38:24 -0600

    Column annotation: name property specifies column name in DB

--- src/main/java/com/sagasoftech/jpa/hibernate/entity/Course.java
+++ src/main/java/com/sagasoftech/jpa/hibernate/entity/Course.java
@@ -1,5 +1,6 @@
 package com.sagasoftech.jpa.hibernate.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -13,6 +14,7 @@ public class Course {
 	@GeneratedValue
 	private Long id;
 
+	@Column(name="NAME")
 	private String name;
 
 	protected Course() {
